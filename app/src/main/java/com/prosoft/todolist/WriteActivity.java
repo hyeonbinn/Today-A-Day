4f2eaef584e7e04142ecd85e5c0906cdeb504290
hyeonbin <basilla0617@naver.com>
2023-12-08 02:41:06 +0900

    [FEATURE] 할 일 체크박스 리스트로 구현

--- app/src/main/java/com/prosoft/todolist/WriteActivity.java
+++ app/src/main/java/com/prosoft/todolist/WriteActivity.java
@@ -1,131 +1,18 @@
-package com.prosoft.todolist;//package com.prosoft.todolist;
-//
-//import android.content.Context;
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.Menu;
-//import android.view.MenuInflater;
-//import android.view.MenuItem;
-//import android.widget.EditText;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import java.io.FileInputStream;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//import androidx.appcompat.app.AppCompatDelegate;
-//
-//
-//public class WriteActivity extends AppCompatActivity {
-//
-//    TextView writeDay;
-//    EditText edtToDoList;
-//
-//    String fileName;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // 추가
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.active_write);
-//        setTitle("할 일 작성");
-//
-//        String year, monthday;
-//        String fnames = "";
-//
-//        edtToDoList = findViewById(R.id.edtDiary);
-//
-//        writeDay = findViewById(R.id.writeDay);
-//        Intent intent = getIntent();
-//        year = intent.getStringExtra("years");
-//        monthday = intent.getStringExtra("monthdays");
-//        writeDay.setText(year + " " + monthday);
-//        // 파일 이름을 그대로 사용하도록 수정
-//        fileName = writeDay.getText().toString() + ".txt";
-//        String str = readToDoList(fileName);
-//        edtToDoList.setText(str);
-//    }
-//
-//    String readToDoList(String fName) {
-//        String ToDoListStr = null;
-//        FileInputStream inFs;
-//        try {
-//            inFs = openFileInput(fName);
-//            byte[] txt = new byte[inFs.available()];
-//            inFs.read(txt);
-//
-//            ToDoListStr = new String(txt).trim();
-//            inFs.close();
-//            if (ToDoListStr.equals("")) {
-//                // 할 일이 없을 때
-//            } else {
-//                // 할 일이 있을 때
-//                setTitle("할 일 수정");
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return ToDoListStr;
-//    }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        super.onCreateOptionsMenu(menu);
-//        MenuInflater mInflater = getMenuInflater();
-//        mInflater.inflate(R.menu.save_menu, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.saveMenu:
-//                saveDiary(fileName);
-//                return true;
-//        }
-//        return false;
-//    }
-//
-//    // 할 일 저장하는 메소드
-//    private void saveDiary(String fileName) {
-//        FileOutputStream fos = null;
-//
-//        try {
-//            fos = openFileOutput(fileName, Context.MODE_PRIVATE);
-//            String content = edtToDoList.getText().toString();
-//
-//            fos.write(content.getBytes());
-//            fos.flush();
-//            fos.close();
-//
-//            Toast.makeText(getApplicationContext(), "할 일을 저장했습니다.", Toast.LENGTH_SHORT).show();
-//            // 현재 액티비티를 종료하고 이전 액티비티로 돌아가도록 수정
-//            finish();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            Toast.makeText(getApplicationContext(), "오류가 발생하였습니다!", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//}
+package com.prosoft.todolist;
 
-//import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -133,36 +20,71 @@ import java.io.IOException;
 public class WriteActivity extends AppCompatActivity {
 
     TextView writeDay;
-    EditText edtToDoList;
-    CheckBox checkBoxTodo; // 체크박스 참조 변수 추가
+    EditText feedback;
+    LinearLayout checkListLayout;
 
     String fileName;
 
-//    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // 추가
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.active_write);
         setTitle("할 일 작성");
 
         String year, monthday;
 
-        edtToDoList = findViewById(R.id.edtDiary);
-        checkBoxTodo = findViewById(R.id.checkBoxTodo); // 체크박스 참조 변수 초기화
+        feedback = findViewById(R.id.feedback);
+        checkListLayout = findViewById(R.id.checkListLayout);
 
         writeDay = findViewById(R.id.writeDay);
         Intent intent = getIntent();
         year = intent.getStringExtra("years");
         monthday = intent.getStringExtra("monthdays");
         writeDay.setText(year + " " + monthday);
-        // 파일 이름을 그대로 사용하도록 수정
         fileName = writeDay.getText().toString() + ".txt";
         String str = readToDoList(fileName);
-        edtToDoList.setText(str);
+        feedback.setText(str);
+
+        // 할 일을 동적으로 추가
+        findViewById(R.id.btnAddTodo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTodo();
+            }
+        });
     }
 
-    String readToDoList(String fName) {
+    private void addTodo() {
+        LinearLayout taskLayout = new LinearLayout(this);
+        taskLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        taskLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+        CheckBox checkBox = new CheckBox(this);
+        checkBox.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        taskLayout.addView(checkBox);
+
+        // EditText 추가
+        EditText todoEditText = new EditText(this);
+        todoEditText.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        todoEditText.setHint("할 일을 입력하세요.");
+        todoEditText.setTextSize(20);
+        taskLayout.addView(todoEditText);
+
+        // 생성된 레이아웃을 기존의 할 일 목록 레이아웃에 추가
+        checkListLayout.addView(taskLayout);
+    }
+
+    private String readToDoList(String fName) {
         String ToDoListStr = null;
         FileInputStream inFs;
         try {
@@ -172,14 +94,6 @@ public class WriteActivity extends AppCompatActivity {
 
             ToDoListStr = new String(txt).trim();
             inFs.close();
-            if (ToDoListStr.equals("")) {
-                // 할 일이 없을 때
-                checkBoxTodo.setChecked(false); // 체크박스를 체크하지 않은 상태로 설정
-            } else {
-                // 할 일이 있을 때
-                checkBoxTodo.setChecked(true); // 체크박스를 체크한 상태로 설정
-                setTitle("할 일 수정");
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -204,20 +118,34 @@ public class WriteActivity extends AppCompatActivity {
         return false;
     }
 
-    // 할 일 저장하는 메소드
     private void saveDiary(String fileName) {
         FileOutputStream fos = null;
 
         try {
-            fos = openFileOutput(fileName, Context.MODE_PRIVATE);
-            String content = edtToDoList.getText().toString();
+            fos = openFileOutput(fileName, MODE_PRIVATE);
+            StringBuilder content = new StringBuilder();
 
-            fos.write(content.getBytes());
+            // 체크박스와 텍스트를 한 줄로 저장
+            for (int i = 0; i < checkListLayout.getChildCount(); i++) {
+                LinearLayout taskLayout = (LinearLayout) checkListLayout.getChildAt(i);
+                CheckBox checkBox = (CheckBox) taskLayout.getChildAt(0);
+                EditText taskEditText = (EditText) taskLayout.getChildAt(1);
+
+                String task = taskEditText.getText().toString();
+                String isChecked = checkBox.isChecked() ? "1" : "0"; // 1은 체크, 0은 체크 해제
+
+                content.append(isChecked).append("|").append(task).append("\n");
+            }
+
+            fos.write(content.toString().getBytes());
+
+            // 하루 피드백 추가
+            content.append("\n").append(feedback.getText().toString());
+
             fos.flush();
             fos.close();
 
-            Toast.makeText(getApplicationContext(), "할 일을 저장했습니다.", Toast.LENGTH_SHORT).show();
-            // 현재 액티비티를 종료하고 이전 액티비티로 돌아가도록 수정
+            Toast.makeText(getApplicationContext(), "할 일 및 피드백을 저장했습니다.", Toast.LENGTH_SHORT).show();
             finish();
 
         } catch (Exception e) {
